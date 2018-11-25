d781284a0e42d5d4fb131c35218be22b8d45938f
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2018-11-25 18:11:37 +0200

    Initial commit.

--- WebService/App_Start/WebApiConfig.cs
+++ WebService/App_Start/WebApiConfig.cs
@@ -1,24 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace WebService
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+			config.EnableCors();
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			// Web API routes
+			// config.MapHttpAttributeRoutes(); // We do not use HttpAttribute here.
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
 }
---
+++ WebService/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebService.Controllers
+{
+	[EnableCors(origins: "http://localhost:29438", headers: "*", methods: "*")]
+	public class TestController : ApiController
+	{
+		// GET: api/Test
+		public HttpResponseMessage Get()
+		{
+			return new HttpResponseMessage
+			{
+				Content = new StringContent("GET: Test message")
+			};
+		}
+
+		// POST: api/Test
+		public HttpResponseMessage Post([FromBody]string value)
+		{
+			return new HttpResponseMessage
+			{
+				Content = new StringContent("POST: Test message")
+			};
+		}
+
+		// PUT: api/Test
+		public HttpResponseMessage Put([FromBody]string value)
+		{
+			return new HttpResponseMessage
+			{
+				Content = new StringContent("PUT: Test message")
+			};
+		}
+	}
+}
